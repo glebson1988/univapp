b3f1d5bfce6ceeb6f31b5af7743120afe400eb03
glebson1988 <glebson1988@yandex.ru>
2020-04-30 10:28:36 +0300

    add students

---
+++ app/models/student.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Student < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  before_save { self.email = email.downcase }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_429_121_207) do
+ActiveRecord::Schema.define(version: 20_200_430_071_246) do
   create_table 'courses', force: :cascade do |t|
     t.string 'short_name'
     t.string 'name'
     t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'students', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
