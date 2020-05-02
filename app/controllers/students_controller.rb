fc3ab4518d5368e6e34a585a82e9d73092086c32
glebson1988 <glebson1988@yandex.ru>
2020-05-02 08:10:21 +0300

    student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,10 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
