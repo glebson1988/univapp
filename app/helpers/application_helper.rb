174e7dfb699d237a1b280170a1a914048fa337ad
glebson1988 <glebson1988@yandex.ru>
2020-05-05 00:00:58 +0300

    login/logout functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def session_link
+    if logged_in?
+      link_to 'logout', logout_path, method: :delete
+    else
+      link_to 'login', login_path
+    end
+  end
 end
